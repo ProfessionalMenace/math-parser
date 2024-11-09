b524691c1d4234c32807da89117c0955d1ae15d8
ProfessionalMenace <vondracekadam00@gmail.com>
2024-11-09 20:37:11 +0100

    Created temp layout of main

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,20 @@
 const std = @import("std");
 const parser = @import("parser.zig");
 
-pub fn main() void {}
+pub fn main() !void {
+    const stdin = std.io.getStdIn();
+    const reader = stdin.reader();
+    var buffer: [512]u8 = undefined;
+    _ = try reader.readUntilDelimiterOrEof(&buffer, '\n');
+    // To do:
+    // handle returning error
+    // add checks
+    //     valid parenthesis
+    //     valid operators
+    //     valid numbers
+    // handle invalid input
+    // preprocessing
+    // processing
+    // cleanup
+    std.debug.print("Expression: {s}\n", .{buffer});
+}
