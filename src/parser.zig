cbe7a7d540ff0a9260700c0c98749191c680e816
ProfessionalMenace <vondracekadam00@gmail.com>
2024-11-17 23:18:41 +0100

    Update parser.zig
    added slicer for equations

--- src/parser.zig
+++ src/parser.zig
@@ -1,6 +1,25 @@
 const std = @import("std");
 const parseInt = std.fmt.parseInt;
 const parseFloat = std.fmt.parseFloat;
+const classifier = @import("classifier.zig");
+const symb = classifier.math_symbols;
+const classify = classifier.classifier;
+
+pub fn slicer(expression: []u8) void {
+    var last: symb = symb.unknown;
+    var curr: symb = undefined;
+    var count: usize = 0;
+    for (expression, 0..) |ch, i| {
+        curr = classify(ch);
+        if (curr != last) {
+            last = curr;
+            count = 0;
+        } else {
+            count += 1;
+        }
+        std.debug.print("{s}\n", .{expression[i - count .. i + 1]});
+    }
+}
 
 pub fn parse_int(slice: []u8) !i64 {
     return try parseInt(i64, slice, 10);
@@ -12,4 +31,9 @@ test "Test parse" {
     std.debug.print("{any}\n", .{ok});
     const err = parse_int(&string);
     std.debug.print("{any}\n", .{err});
+}
+
+test "Basic lhs addition" {
+    var string = [_]u8{ '1', '+', '1', '2', '=', '1', '3' };
+    slicer(&string);
 }
