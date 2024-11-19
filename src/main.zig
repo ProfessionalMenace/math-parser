d0565e5cce6767073464cc6c6e8da73043130ca1
ProfessionalMenace <vondracekadam00@gmail.com>
2024-11-19 22:15:29 +0100

    Update main.zig
    updated main

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,23 @@
 const std = @import("std");
+const pproces = @import("preprocessor.zig");
 const parser = @import("parser.zig");
 
 pub fn main() !void {
     const stdin = std.io.getStdIn();
     const reader = stdin.reader();
+    var input: [512]u8 = undefined;
     var buffer: [512]u8 = undefined;
-    _ = try reader.readUntilDelimiterOrEof(&buffer, '\n');
+    @memset(&buffer, 0);
+    const bytes_read = try reader.readUntilDelimiterOrEof(&input, '\n');
+    _ = bytes_read;
+    pproces.remove_unknown(&input);
+    const pvalid = pproces.validate_parenthesis(&input, &buffer);
+    if (pvalid) {
+        std.debug.print("Parenthesies are valid\n", .{});
+    } else {
+        std.debug.print("Parenthesies are invalid\n", .{});
+    }
+
     // To do:
     // handle returning error
     // add checks
@@ -16,5 +28,5 @@ pub fn main() !void {
     // preprocessing
     // processing
     // cleanup
-    std.debug.print("Expression: {s}\n", .{buffer});
+    std.debug.print("Expression: {s}\n", .{input});
 }
